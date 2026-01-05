992156cc00ca5f7b2c0253ed09517509960ca1d4
jovan <jovan@DESKTOP-V88K6I3>
2026-01-05 10:12:28 +0800

    Added Domain

---
+++ SportsVenueBooking/Domain/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsVenueBooking.Domain
+{
+    public class Booking : BaseDomainModel
+    {
+        public DateTime TimeSlot { get; set; }
+        public string? Sport { get; set; }
+        public int VenueId { get; set; }
+        public int CustomerId { get; set; }
+        public int PaymentId { get; set; }
+    }
+}
---
+++ SportsVenueBooking/Domain/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsVenueBooking.Domain
+{
+    public class Customer : BaseDomainModel
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int ContactNumber { get; set; }
+    }
+}
